ed43622628e2b8fd18f2f7de65696edd9ea90db4
Tshepo <maubane808@gmail.com>
2025-04-01 21:11:10 +0200

    register added

---
+++ django/register.py
@@ -0,0 +1,22 @@
+
+
+def register(request):
+    '''
+    Method registers a new user account on the database.
+    :param object request: User inputs data into the system.
+    :returns: A message to the user confirming account creation.
+    :return type: str.
+    '''
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        context = {'form': form}
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Welcome {username}! "
+                                f"Your account has been created. You may login!")
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+        context = {'form': form} 
+    return render(request, 'user_auth/register.html', context)
